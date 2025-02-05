dc984a9555a664658ccfd46d296528ad44e60a7e
Aphene101 <karen.saleh.kitten@gmail.com>
2025-02-05 10:37:14 +0200

    Finished the first chunk of js exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...num) {
+    let newArray = []; 
+    for (let i = 0; i < array.length; i++) {
+        if (!num.includes(array[i])) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (start < 0 || end < 0 ||
+         !Number.isInteger(start) || !Number.isInteger(end) ||
+        typeof start === 'string' || typeof end === 'string') {
+        return 'ERROR';
+    }
+    if (start > end) {
+        for (let i = end; i < start + 1; i++) {
+        sum += i;
+        }
+        return sum;
+    }
+    if (end > start) {
+        for (let i = start; i < end + 1; i++) {
+        sum += i;
+        }
+        return sum;
+    }
+    
 };
 
 // Do not edit below this line
